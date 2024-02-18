1c352298b93ac3e8bf76329a6f801f70f1fb12fb
Nubassim <nubassim17@gmail.com>
2024-02-19 01:49:19 +0600

    home to playground page done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,1 +1,16 @@
-function play
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+    console.log('Your Random Alphabet', alphabet);
+}
+
+function play(){
+    const homeSection = document.getElementById('home-screen');
+    homeSection.classList.add ('hidden');
+    // console.log(homeSection)
+
+    const playGroundSection = document.getElementById('play-ground');
+    playGroundSection.classList.remove('hidden');
+    // console.log(playGroundSection.classList);
+
+    continueGame();
+}
---
+++ scripts/utility.js
@@ -0,0 +1,10 @@
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    return alphabets;
+}
